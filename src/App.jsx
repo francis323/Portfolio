a4d862d83648d36330f545e4ff65464d108bd7c5
Jeranguz <JeremyGD997@hotmail.com>
2026-03-10 12:48:59 -0600

    feat: update dependencies and enhance App component with routing and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ChevronDown } from "lucide-react"
+import { motion } from "framer-motion"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='bg-amber-600'>
-        Hello world
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div className='bg-amber-600'>
+            <ChevronDown />
+            <motion.h2
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.4 }}>
+              Hello World
+            </motion.h2>
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
